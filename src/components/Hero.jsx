a5bcfae125334fba8a6434c04db1bc41008c3088
priyanshu <test@gmail.com>
2023-11-11 15:14:29 +0530

    Header-done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,6 +17,17 @@ function Hero() {
           GitHub
         </button>
         </nav>
+
+        <h1 className='head_text'>
+          {/* max-md:hidden means break the line at smaller screens */}
+        Summarize Articles with <br className='max-md:hidden' />
+        <span className='orange_gradient '>OpenAI GPT-4</span>
+      </h1>
+      <h2 className='desc'>
+        Simplify your reading with Summize, an open-source article summarizer
+        that transforms lengthy articles into clear and concise summaries
+      </h2>
+
     </header>
   )
 }
